206e87010c195e6e8f5aa951e95cc2eb69923bbf
Yogesh More <135803626+iamyogeshmore@users.noreply.github.com>
2023-07-26 23:19:41 +0530

    Added: A few quotes and a header and footer were added , and a github link was added to the footer ..

--- script.js
+++ script.js
@@ -14,13 +14,19 @@ document.addEventListener("DOMContentLoaded", function () {
     "Hard work is worthless for those that don’t believe in themselves.– Naruto Uzumaki",
     "Be someone’s light when they are hopeless. – Violet evergarden",
     "Don’t worry about what other people think. Hold your head up high and plunge forward – Izuku Midoriya ",
-    "THE MOMENT PEOPLE COME TO KNOW LOVE, THEY RUN THE RISK OF CARRYING HATE. – OBITO UCHIHA",
+    "The moment people come to know Love, They run the risk of carrying Hate. - Obito Uchiha",
     "The longer you live… The more you realize that reality is just made of pain, suffering, and emptiness. – Madara Uchiha",
     "It’s just pathetic to give up on something before you even give it a shot. – Reiko Mikami",
     "Simplicity is the easiest path to true beauty. – Seishuu Handa",
     "Power comes in response to a need, not a desire. You have to create that need. – Goku",
     "If you master one, that’s cause for celebration. - Zenitsu Agatsuma",
     "f you can only do one thing, hone it to perfection. Hone it to the utmost limit. - Jigoro Kuwajima",
+    "Learn to treasure your life because unfortunately, it can be taken away from you anytime. – L Lawliet ",
+    "Life is not a game of luck. If you wanna win, work hard. – Sora",
+    "Every journey begins with a single step. We just have to have patience. – Milly Thompson",
+    "Miracles don’t happen by just sitting around. We have to make them happen ourselves. – Kiyoshi Fujino",
+    "Sometimes I do feel like I’m a failure. Like there’s no hope for me. But even so, I’m not gonna give up. Ever! – Izuku Midoriya",
+    "The only thing we’re allowed to do is to believe that we won’t regret the choice we made. – Levi Ackerman"
   ];
 
   const quoteDisplay = document.getElementById("quoteDisplay");
